2a2f003fac7c08e18437b1a2042dd450e556348c
Jasiu Leja <jleja09@gmail.com>
2017-10-18 19:12:45 -0400

    adds componentDidMount function and d3Scripts file

---
+++ client/d3Scripts.js
@@ -0,0 +1,12 @@
+const d3 = require('d3');
+
+const makeChart = () => {
+  var svg = d3.select('svg'),
+    width = +svg.attr('width'),
+    height = +svg.attr('height');
+
+
+
+};
+
+export default makeChart;
